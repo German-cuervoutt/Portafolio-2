33f2514eb7f4fbebe06fe157766048a2be9572b5
German-cuervoutt <0322104023@ut-tijuana.edu.mx>
2024-10-31 18:51:22 -0700

    Delete index/index directory

--- index/index/nosotros.php
+++
@@ -1,9 +0,0 @@
-<?php include "../index/header.php"?>
-    <section>
-        <h2>Conoce Sobre Nosotros</h2>
-        <p>25 years of Experience</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente ratione eaque ullam nesciunt similique placeat, aut vero mollitia minima non adipisci facilis esse quam voluptatibus temporibus doloribus soluta iure nisi!</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam rem ipsam </p>
-    </section>
-
-<?php include "../index/includes" ?>
